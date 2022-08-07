eee416bd404e15751b1bd8e34a80c390fdd144f7
sjnay <samanthajnay@gmail.com>
2022-08-07 09:50:52 -0500

    Boiler plate, pseudo code html & js, make sure everything is connected

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("js loaded"); //test
+
+// global variables - spin button, get random number(1-10) 
+// playGame function that displays random number in each of the "lanes" when spin btn is clicked
+// have each random number correspond to a picture and point system
+// use a class for pictures and point system
+// how to "win"
+// pop ups for certain winning combos
+// reset button
+
+
+
+
+
