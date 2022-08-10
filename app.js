3172670d1206f4dd6a7c1f24be85baffad218888
sjnay <samanthajnay@gmail.com>
2022-08-10 11:22:10 -0500

    Score board and game messages

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ console.log("js loaded"); //test
 // pop ups for certain winning combos
 // reset button
 
-
+//global variables
 const spinBtn=document.getElementById("spin");
 const scoreBoard=document.querySelector(".scoreboard");
 
@@ -17,7 +17,7 @@ const scoreBoard=document.querySelector(".scoreboard");
 function randomNumber(){ 
     return Math.floor(Math.random()*4)
 }
-// finds s lanes in html
+// finds display lanes in html
 function findElement(id){
     return document.getElementById(id);
 
@@ -30,35 +30,45 @@ const pointsLane2 =[3,2,-1,2];
 const itemsLane3 =["Bees","Seeds","Critters","Gnome"];
 const pointsLane3 =[2,1,-2,2];
 
-
-
-
-
-//  function addPoints(points){
-//     return totalPoints += points;
-//  }
-
-
-// function removePoints(points){`
-//         return totalPoints -= points;
-// }
-
 let pointsArray = []
 
-function keepScore(){
+function keepScoreLane1(){
     if (display1.innerHTML === itemsLane1[0]) { //sun
         pointsArray.push(pointsLane1[0]); //3
     } else if (display1.innerHTML === itemsLane1[1]){ //tools
-        pointsArray.push(pointsLane1[1]) // 1
+        pointsArray.push(pointsLane1[1]); // 1
     } else if (display1.innerHTML === itemsLane1[2]){ //soil
-        pointsArray.push(pointsLane1[2]) //1
-    } else {pointsArray.push(pointsLane1[3]); //gnome 2
-    
-    }
+        pointsArray.push(pointsLane1[2]); //1
+    } else {pointsArray.push(pointsLane1[3]);} //gnome
 }
 
- 
+function keepScoreLane2(){
+    if (display2.innerHTML === itemsLane2[0]){ //water
+        pointsArray.push(pointsLane2[0]); //3
+    } else if (display2.innerHTML === itemsLane2[1]){ //butterfly
+        pointsArray.push(pointsLane2[1]); //2
+    } else if (display2.innerHTML === itemsLane2[2]){ //bugs
+        pointsArray.push(pointsLane2[2]); // -1
+    } else {pointsArray.push(pointsLane1[3]);} // gnome
+}
 
+function keepScoreLane3(){
+    if (display3.innerHTML === itemsLane3[0]){ //bees
+        pointsArray.push(pointsLane3[0])// 2
+    } else if (display3.innerHTML === itemsLane3[1]){ // seeds
+        pointsArray.push(pointsLane3[1])// 1
+    } else if (display3.innerHTML === itemsLane3[2]){ // critters
+        pointsArray.push(pointsLane3[2]) //-2
+    } else  pointsArray.push(pointsLane1[3]); //gnome 
+    
+}
+
+function gameOver(){
+    if (display1.innerHTML === itemsLane1[2] && display2.innerHTML === itemsLane2[2] && display3.innerHTML ===itemsLane3[2]){
+        findElement("game-messages").innerText =  "Aw shucks! Your garden is dead! Game over!"
+    } else {findElement("game-messages").innerText =""};
+
+}
 
 spinBtn.addEventListener("click",function(event){
     event.preventDefault()
@@ -71,7 +81,10 @@ spinBtn.addEventListener("click",function(event){
     display2.innerHTML = `${itemsLane2[randomNumber()]}`;
     display3.innerHTML = `${itemsLane3[randomNumber()]}`;
 
-    keepScore(); 
+    keepScoreLane1(); 
+    keepScoreLane2();
+    keepScoreLane3();
+    gameOver();
     const initialValue = 0;
     let sumWithInitial = pointsArray.reduce(
       (previousValue, currentValue) => previousValue + currentValue,
