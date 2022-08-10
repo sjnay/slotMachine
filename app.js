0627785eb4f1f3c1daf40254716e71498b56d163
sjnay <samanthajnay@gmail.com>
2022-08-10 09:31:12 -0500

    Scoreboard WORKS

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ console.log("js loaded"); //test
 
 
 const spinBtn=document.getElementById("spin");
-
+const scoreBoard=document.querySelector(".scoreboard");
 
 
 // generate a random number between 0&4
@@ -23,19 +23,46 @@ function findElement(id){
 
 }
 
-const itemsLane1 =["Sun","Tools","Soil","Gnome"]
-const pointsLane1 =[3,1,1,2]
-const itemsLane2 =["Water","Butterfly","Bugs","Gnome"]
-const pointsLane2 =[3,2,-1,2]
-const itemsLane3 =["Bees","Seeds","Critters","Gnome"]
-const pointsLane3 =[2,1,-2,2]
+const itemsLane1 =["Sun","Tools","Soil","Gnome"];
+const pointsLane1 =[3,1,1,2];
+const itemsLane2 =["Water","Butterfly","Bugs","Gnome"];
+const pointsLane2 =[3,2,-1,2];
+const itemsLane3 =["Bees","Seeds","Critters","Gnome"];
+const pointsLane3 =[2,1,-2,2];
 
 
 
 
 
-function playGame(){
+//  function addPoints(points){
+//     return totalPoints += points;
+//  }
+
+
+// function removePoints(points){`
+//         return totalPoints -= points;
+// }
+
+let pointsArray = []
+
+function keepScore(){
+    if (display1.innerHTML === itemsLane1[0]) { //sun
+        pointsArray.push(pointsLane1[0]); //3
+    } else if (display1.innerHTML === itemsLane1[1]){ //tools
+        pointsArray.push(pointsLane1[1]) // 1
+    } else if (display1.innerHTML === itemsLane1[2]){ //soil
+        pointsArray.push(pointsLane1[2]) //1
+    } else {pointsArray.push(pointsLane1[3]); //gnome 2
     
+    }
+}
+
+ 
+
+
+spinBtn.addEventListener("click",function(event){
+    event.preventDefault()
+
     const display1 = findElement("display1");
     const display2 = findElement("display2");
     const display3 = findElement("display3");
@@ -43,24 +70,41 @@ function playGame(){
     display1.innerHTML = `${itemsLane1[randomNumber()]}`;
     display2.innerHTML = `${itemsLane2[randomNumber()]}`;
     display3.innerHTML = `${itemsLane3[randomNumber()]}`;
-}
+
+    keepScore(); 
+    const initialValue = 0;
+    let sumWithInitial = pointsArray.reduce(
+      (previousValue, currentValue) => previousValue + currentValue,
+      initialValue
+    );
+    
+    scoreBoard.innerHTML=sumWithInitial;
+    
+    
+})
+
+
+
+console.log(pointsArray);
+
+
+
+ 
+ 
 
 
 
 
-let totalPoints=0;
 
-function addPoints(points){
-   totalPoints += points;    
-}
 
-function removePoints(points){
-    totalPoints -= points;
-}
 
-spinBtn.addEventListener("click",playGame());
 
-document.querySelector(".game-overview").innerHTML = keepScore();
+
+
+
+
+
+
 
 
 
