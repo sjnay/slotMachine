28c2ef142bff573e80acfc86ea8aaf0e215b5f4b
sjnay <samanthajnay@gmail.com>
2022-08-12 02:01:50 -0500

    Complete

--- app.js
+++ app.js
@@ -10,14 +10,13 @@ console.log("js loaded"); //test
 
 //global variables
 const spinBtn=document.getElementById("spin");
-const scoreBoard=document.querySelector("#total-points")
-const newGame=document.getElementById("new-game")
+const newGame=document.getElementById("new-game");
 
 
 
 // generate a random number between 0&4
 function randomNumber(){ 
-    return Math.floor(Math.random()*4)
+    return Math.floor(Math.random()*4);
 }
 // finds display lanes in html
 function findElement(id){
@@ -27,7 +26,7 @@ function findElement(id){
 
 const itemsLane1 =["Sun","Tools","Weeds","Gnome"];
 const pointsLane1 =[3,1,-2,2];
-const itemsLane2 =["Water","Butterfly","Bugs","Gloves"];
+const itemsLane2 =["Water","Butterfly","Pests","Gloves"];
 const pointsLane2 =[3,2,-1,1];
 const itemsLane3 =["Bees","Seeds","Critters","Soil"];
 const pointsLane3 =[2,1,-2,1];
@@ -65,29 +64,21 @@ function keepScoreLane3(){
     
 }
 
-function motivate(){
-    if (display1.innerHTML === itemsLane1[2] && display2.innerHTML === itemsLane2[2] && display3.innerHTML ===itemsLane3[2]){
-        findElement("game-messages").innerHTML="Your garden is dead! Game Over!"
-    } else if(display1.innerHTML === itemsLane1[0] && display2.innerHTML === itemsLane2[0] && display3.innerHTML === itemsLane3[1]) {
-        findElement("game-messages").innerHTML="Essential items for your garden!";
-    } else if(display1.innerHTML === itemsLane1[0] && display2.innerHTML === itemsLane2[1] && display3.innerHTML === itemsLane3[0]){
-        findElement("game-messages").innerHTML="Keep going! Your garden is thriving! "
-    } else {findElement("game-messages").innerHTML="Get to growing!";}
-}
+
 
 function gameOverCombo(){
     if (display1.innerHTML === itemsLane1[2] && display2.innerHTML === itemsLane2[2] && display3.innerHTML ===itemsLane3[2]){
         display1.innerHTML="-";
         display2.innerHTML="-";
         display3.innerHTML="-";
-        window.confirm(`Your garden is infested with ${itemsLane1[2]}, ${itemsLane2[2]}, and ${itemsLane3[2]}\n Oh my!\n Play Again!`);
+        findElement("crops-grown").innerHTML=`Your garden is infested with ${itemsLane1[2]}, ${itemsLane2[2]}, and ${itemsLane3[2]} oh my!`;
         pointsArray.splice(0,pointsArray.length);
         findElement("garden-item-1").innerHTML="";
         findElement("garden-item-2").innerHTML="";
         findElement("garden-item-3").innerHTML="";
-        findElement("garden-item-4").innerHTML="Your garden is empty!";
-        findElement("points-to-crop").innerHTML="Bummer!";
-        scoreBoard.innerHTML="";
+        findElement("garden-item-4").innerHTML="";
+        findElement("points-to-crop").innerHTML="Bummer! Your garden is dead!\nTry again!";
+        
     }
 }
 
@@ -105,10 +96,10 @@ spinBtn.addEventListener("click",function(event){
     keepScoreLane1(); 
     keepScoreLane2();
     keepScoreLane3();
-    motivate();
+    
     
     // keeping score by pushing each value associated with them items to an array and using reduce 
-    // function to have a running total
+    // function to keep a running total
     const initialValue = 0;
     let totalPoints = pointsArray.reduce(
       (previousValue, currentValue) => previousValue + currentValue,
@@ -118,24 +109,24 @@ spinBtn.addEventListener("click",function(event){
     
     // player status and point up dates
     
-    if(totalPoints<=50){
-        findElement("garden-item-4").innerHTML="Your garden is empty!"   
-    findElement("points-to-crop").innerHTML=`You are ${50-totalPoints} points from your first crop, ${crops[0]}!`
+    if(totalPoints<50){
+    findElement("crops-grown").innerHTML="Your garden is empty!"   
+    findElement("points-to-crop").innerHTML=`You are ${50-totalPoints} points from your first crop, <font color=#ba3c3c>${crops[0]}</font>!`
     
     } else if (totalPoints >= 50 && totalPoints < 100){
-    findElement("garden-item-4").innerHTML=`Nice! You harvested ${crops[0]}! `
-    findElement("points-to-crop").innerHTML=`You are ${100-totalPoints} points from growing ${crops[1]}!`
+    findElement("crops-grown").innerHTML=""
+    findElement("garden-item-4").innerHTML=`Nice! You harvested <font color=#ba3c3c> ${crops[0]}</font>! `
+    findElement("points-to-crop").innerHTML=`You are ${100-totalPoints} points from growing <font color=#eaa221>${crops[1]}</font>!`
     
     } else if (totalPoints >=100 && totalPoints < 150){
-    findElement("garden-item-3").innerHTML=`You are on a roll! Now you have ${crops[1]}!`
-    findElement("points-to-crop").innerHTML=`You are ${150-totalPoints} points from harvesting ${crops[2]}!`
+    findElement("garden-item-3").innerHTML=`You are on a roll! Now you have <font color=#eaa221>${crops[1]}</font>!`
+    findElement("points-to-crop").innerHTML=`You are ${150-totalPoints} points from harvesting <font color=#5F8575>${crops[2]}</font>!`
     
     } else if (totalPoints >= 150 && totalPoints < 200){
-    findElement("garden-item-2").innerHTML=`You did it! You grew ${crops[2]}`
-    findElement("points-to-crop").innerHTML=`You are ${200-totalPoints} points from successfully growing ${crops[3]}!`
+    findElement("garden-item-2").innerHTML=`You did it! You grew <font color=#5F8575>${crops[2]}</font>!`
+    findElement("points-to-crop").innerHTML=`You are ${200-totalPoints} points from growing<font color=FFE62B> ${crops[3]}</font>!`
     
     } else if (totalPoints > 200 && totalPoints < 209){
-    findElement("garden-item-1").innerHTML=`Look at you grow! You did it, you grew ${crops[3]}!`
     findElement("points-to-crop").innerHTML=""
     findElement("crops-grown").innerHTML="You did it! Your garden is complete!";
     pointsArray.splice(0,pointsArray.length);
@@ -143,7 +134,6 @@ spinBtn.addEventListener("click",function(event){
     findElement("garden-item-2").innerHTML="";
     findElement("garden-item-3").innerHTML="";
     findElement("garden-item-4").innerHTML="";
-    findElement("game-messages").innerHTML="Nice work!"
     } 
 
     gameOverCombo();
